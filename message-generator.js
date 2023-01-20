2a4d2fd6b46e1f3ae4e5aff128fbd9e982b65e2f
KLCD <matt.flick.101@gmail.com>
2023-01-19 20:21:57 -0500

    Added functionailty to output the final message

--- message-generator.js
+++ message-generator.js
@@ -1,1 +1,12 @@
-console.log("test2");
+//Store the different components of the message in 3 different arrays
+let types = ["Error", "Warning", "Fun Fact"];
+let subjects = ["Water", "Penguins", "Time"];
+let substance = ["NEARING THE END", "hating potatoes", "now not liking you, you in particular. Yes the person reading this, you"];
+
+//A method to quickly get a random number from 0 <= random < n
+function getRandom(n){
+    return Math.floor(Math.random()*n);
+}
+
+//Format and output a string randomly combined from the components
+console.log(`${types[getRandom(3)]}: ${subjects[getRandom(3)]} is ${substance[getRandom(3)]}`);
